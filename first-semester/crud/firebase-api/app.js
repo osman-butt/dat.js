df6428df28441becba356360e8497b5ab1c2edc1
osman-butt <butt.osman@gmail.com>
2023-04-18 14:54:04 +0200

    adding users data

--- first-semester/crud/firebase-api/app.js
+++ first-semester/crud/firebase-api/app.js
@@ -5,11 +5,14 @@ const endpoint =
   "https://rest-api-intro-default-rtdb.europe-west1.firebasedatabase.app";
 
 const query = "posts";
+const queryUsers = "users";
 
 async function initApp() {
   console.log("initApp: app.js is running 🎉");
   const posts = await getPosts();
+  const users = await getUsers();
   displayPosts(posts);
+  displayUsers(users);
 }
 
 async function displayPosts(posts) {
@@ -18,6 +21,16 @@ async function displayPosts(posts) {
   if (posts.length > 0) {
     showGrid();
     posts.forEach(displayPost);
+  } else {
+    hideGrid();
+  }
+}
+async function displayUsers(users) {
+  console.log("---displayUsers---");
+  document.querySelector("#grid-users").innerHTML = "";
+  if (users.length > 0) {
+    showGrid();
+    users.forEach(displayUser);
   } else {
     hideGrid();
   }
@@ -55,6 +68,16 @@ async function getPosts() {
   const data = await response.json();
   const posts = preparePostData(data);
   return posts;
+}
+
+// === READ (GET) === //
+async function getUsers() {
+  console.log("---getUsers---");
+  const response = await fetch(`${endpoint}/${queryUsers}.json`);
+  const data = await response.json();
+  const users = preparePostData(data);
+  console.log(users);
+  return users;
 }
 
 // === DELETE (DELETE) === //
@@ -118,6 +141,33 @@ function displayPost(post) {
     .addEventListener("click", function () {
       deletePost(post);
     });
+}
+function displayUser(users) {
+  console.log("---displayUsers---");
+  const postHtml = /*html*/ `
+            <article class="grid-item">
+          <img
+            src="${users.image}"
+            alt=""
+          />
+          <h2 style="text-align: center;">${users.name}</h2>
+          <h3 style="text-align: center;">${users.title}</h3>
+          <button id="delete-btn">DELETE</button>
+        </article>
+  `;
+  document
+    .querySelector("#grid-users")
+    .insertAdjacentHTML("beforeend", postHtml);
+  // document
+  //   .querySelector("#grid-users article:last-child img")
+  //   .addEventListener("click", function () {
+  //     showModal(users);
+  //   });
+  // document
+  //   .querySelector("#grid-users article:last-child button")
+  //   .addEventListener("click", function () {
+  //     deletePost(users);
+  //   });
 }
 
 function showModal(post) {
