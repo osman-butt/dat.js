f43ce72c08e96957362f134c79ffaa64623b8641
osman-butt <butt.osman@gmail.com>
2023-04-18 02:06:41 +0200

    getPosts+displayPost added

--- first-semester/crud/firebase-api/app.js
+++ first-semester/crud/firebase-api/app.js
@@ -1,2 +1,45 @@
 "use strict";
 window.addEventListener("load", initApp);
+
+const endpoint =
+  "https://rest-api-intro-default-rtdb.europe-west1.firebasedatabase.app";
+async function initApp() {
+  console.log("initApp: app.js is running 🎉");
+  const posts = await getPosts();
+  posts.forEach(displayPost);
+  console.log(posts);
+}
+
+async function getPosts() {
+  console.log("---getZipData---");
+  const response = await fetch(`${endpoint}/posts.json`);
+  const data = await response.json();
+  const posts = preparePostData(data);
+  return posts;
+}
+
+// Converts object of objects to array of objects
+function preparePostData(dataObject) {
+  console.log("---preparePostData---");
+  const posts = [];
+  for (const key in dataObject) {
+    const post = dataObject[key];
+    post.id = key;
+    posts.push(post);
+  }
+  return posts;
+}
+
+function displayPost(post) {
+  console.log("---displayPost---");
+  const postHtml = /*html*/ `
+            <article class="grid-item">
+          <img
+            src="${post.image}"
+            alt=""
+          />
+          <h2 style="text-align: center;">${post.title}</h2>
+        </article>
+  `;
+  document.querySelector("#grid").insertAdjacentHTML("beforeend", postHtml);
+}
