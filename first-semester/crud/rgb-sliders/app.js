e7cda83f5b6ae7b0d5aad4c0ecbd18d8bd722560
osman-butt <butt.osman@gmail.com>
2023-04-23 19:35:12 +0200

    rgb-slider display function

--- first-semester/crud/rgb-sliders/app.js
+++ first-semester/crud/rgb-sliders/app.js
@@ -4,4 +4,22 @@ window.addEventListener("load", initApp);
 
 function initApp() {
   console.log("initApp: app.js is running 🎉");
+  changeColor();
+  document.querySelector("#fred").addEventListener("change", displayColor);
+  document.querySelector("#fgreen").addEventListener("change", displayColor);
+  document.querySelector("#fblue").addEventListener("change", displayColor);
+}
+
+function displayColor(event) {
+  document.querySelector("#" + this.id + "output").textContent = this.value;
+  changeColor();
+}
+
+function changeColor() {
+  const red = document.querySelector("#fredoutput").textContent;
+  const green = document.querySelector("#fgreenoutput").textContent;
+  const blue = document.querySelector("#fblueoutput").textContent;
+  console.log(red + ", " + green + ", " + blue);
+  document.querySelector("#rgboutput").style.backgroundColor =
+    "rgb(" + red + "," + green + ", " + blue + ")";
 }
